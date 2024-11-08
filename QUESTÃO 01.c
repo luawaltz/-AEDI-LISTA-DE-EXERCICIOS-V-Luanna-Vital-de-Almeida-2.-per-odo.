76a97b7cf49bb73d638046771cba2b6680494b09
lua <luannavital.almeida@gmail.com>
2024-11-08 15:10:53 -0300

    QUESTÃO 01.c

---
+++ QUESTÃO 01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Função recursiva para somar números de 1 até n
+int somaRecursiva(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n + somaRecursiva(n - 1);
+}
+
+int main() {
+    int numero;
+
+    // Testar a função com diferentes valores
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        int resultado = somaRecursiva(numero);
+        printf("A soma de 1 até %d é: %d\n", numero, resultado);
+    } else {
+        printf("Por favor, digite um número inteiro positivo.\n");
+    }
+
+    return 0;
+}
